2c3e35b426009fa617ea84d3f5345f4f219e8d52
Arunprabu <harikrishs932@gmail.com>
2021-05-07 00:03:20 +0530

    Checkpoint 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Header from "./common/header/Header.js"
 
 ReactDOM.render(
  <div>
-
+   <Header/>
  </div>,
   document.getElementById('root')
 );
